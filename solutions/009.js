e0b69b89a3075afd8e63c63bbc5e3b6372ca19da
Jessica Salbert <66483878+jessicasalbert@users.noreply.github.com>
2020-12-10 16:29:05 -0500

    Create 009.js

---
+++ solutions/009.js
@@ -0,0 +1,48 @@
+// Palindrome Number
+
+// O(n) time; O(1) space
+
+
+// Solution 1 - convert to string 
+
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    }
+    x = x.toString()
+    let l = 0
+    let r = x.length - 1
+    while (l <= r) {
+        if (x[l] === x[r]) {
+            l++
+            r--
+        } else {
+            return false
+        }
+    }
+    return true
+};
+
+// Solution 2 - use % to add digits to array 
+
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    }
+    const digits = []
+    while (x >= 1) {
+        digits.push(x % 10)
+        x = Math.floor(x / 10)
+    }
+    let l = 0
+    let r = digits.length - 1
+    while (l <= r) {
+        if (digits[l] === digits[r]) {
+            l++
+            r--
+        } else {
+            return false
+        }
+    }
+    return true
+};

d2efc7e319ecd4f0c8a6470bada2730204d1fd24
Jessica Salbert <66483878+jessicasalbert@users.noreply.github.com>
2020-12-10 16:39:17 -0500

    Update 009.js

--- solutions/009.js
+++ solutions/009.js
@@ -1,9 +1,10 @@
 // Palindrome Number
 
-// O(n) time; O(n) space to store string/array
+// O(log(n)) time
 
 
 // Solution 1 - convert to string 
+// O(log(n)) space to store string/array
 
 var isPalindrome = function(x) {
     if (x < 0) {
@@ -24,25 +25,18 @@ var isPalindrome = function(x) {
 };
 
 // Solution 2 - use % to add digits to array 
-
+// O(1) space
 var isPalindrome = function(x) {
     if (x < 0) {
         return false
     }
-    const digits = []
-    while (x >= 1) {
-        digits.push(x % 10)
-        x = Math.floor(x / 10)
+    let current = x
+    let reversed = 0;
+    
+    while (current > 0) {
+        let lastDigit = current % 10
+        reversed = (reversed * 10) + lastDigit
+        current = Math.floor(current / 10)
     }
-    let l = 0
-    let r = digits.length - 1
-    while (l <= r) {
-        if (digits[l] === digits[r]) {
-            l++
-            r--
-        } else {
-            return false
-        }
-    }
-    return true
+    return reversed === x
 };
